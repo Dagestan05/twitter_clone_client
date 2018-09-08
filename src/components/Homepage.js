f0df089908e986f2846b73e35724eaeefb20a382
Dagestan05 <steklofan@gmail.com>
2018-09-08 03:33:58 +0200

    styled messageList in  homepage

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,18 +1,27 @@
-import React from 'react'
-import { Link } from 'react-router-dom';
-import MessageTimeline from './MessageTimeline';
+import React from "react";
+import { Link } from "react-router-dom";
+import MessageTimeline from "./MessageTimeline";
 
-const Homepage = ({currentUser}) => {
-  if(!currentUser.isAuthenticated){
+const Homepage = ({ currentUser }) => {
+  if (!currentUser.isAuthenticated) {
     return (
       <div className="home-hero">
         <h1>Whats up</h1>
         <h4>New here?</h4>
-        <Link to="/signup" className="btn btn-primary">Sign up here</Link>
+        <Link to="/signup" className="btn btn-primary">
+          Sign up here
+        </Link>
       </div>
-    )
+    );
   }
-  return <div>< MessageTimeline /></div>
-}
+  return (
+    <div>
+      <MessageTimeline 
+        profileImageUrl={currentUser.user.profileImageUrl} 
+        username={currentUser.user.username} 
+      />
+    </div>
+  );
+};
 
-export default Homepage
+export default Homepage;
--- src/components/MessageTimeline.js
+++ src/components/MessageTimeline.js
@@ -1,12 +1,18 @@
-import React from 'react'
-import MessageList from "../containers/MessageList"
+import React from "react";
+import MessageList from "../containers/MessageList";
+import UserAside from "./UserAside";
 
-const MessageTimeline = (props) => {
+
+const MessageTimeline = props => {
   return (
     <div className="row">
-      <MessageList/>
+      <UserAside
+        profileImageurl={props.profileImageurl}
+        username={props.username}
+      />
+      <MessageList />
     </div>
-  )
-}
+  );
+};
 
 export default MessageTimeline;
